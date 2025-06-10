0619e75c7c15bb55e295e90b710c7444e4dc0100
gorsbart <gorski.bartosz@icloud.com>
2025-06-11 01:09:35 +0200

    Blink migration

--- config/.config/nvim/lua/plugins/lsp.lua
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -85,78 +85,8 @@ return {
             }
         })
 
-        local kind_icons = {
-          Text = "",
-          Method = "󰆧",
-          Function = "󰊕",
-          Constructor = "",
-          Field = "󰇽",
-          Variable = "󰂡",
-          Class = "󰠱",
-          Interface = "",
-          Module = "",
-          Property = "󰜢",
-          Unit = "",
-          Value = "󰎠",
-          Enum = "",
-          Keyword = "󰌋",
-          Snippet = "",
-          Color = "󰏘",
-          File = "󰈙",
-          Reference = "",
-          Folder = "󰉋",
-          EnumMember = "",
-          Constant = "󰏿",
-          Struct = "",
-          Event = "",
-          Operator = "󰆕",
-          TypeParameter = "󰅲",
-        }
-
-
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lua' },
-                { name = 'nvim_lsp' },
-                { name = 'path' },
-                { name = 'luasnip' }, -- For luasnip users.
-                { name = 'buffer' },
-            }),
-            formatting = {
-              format = function(entry, vim_item)
-                -- Kind icons
-                vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
-                -- Source
-                vim_item.menu = ({
-                  buffer = "[Buf]",
-                  nvim_lsp = "[LSP]",
-                  luasnip = "[Snip]",
-                  nvim_lua = "[Lua]",
-                  latex_symbols = "[TeX]",
-                })[entry.source.name]
-                return vim_item
-              end
-            },
-            experimental = {
-              native_menu = false,
-              ghost_text = true
-            }
-        })
-
         vim.diagnostic.config({
+
             -- update_in_insert = true,
             float = {
                 focusable = false,
