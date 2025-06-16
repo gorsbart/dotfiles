49f2606877bf08b171a64f9e3b34b1221d1e46f5
gorsbart <gorski.bartosz@icloud.com>
2025-06-16 14:48:43 +0200

    Lsp new setup

--- config/.config/nvim/lua/plugins/lsp.lua
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -1,89 +1,22 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
+        "mason-org/mason.nvim",
+        "mason-org/mason-lspconfig.nvim",
         "j-hui/fidget.nvim",
     },
     config = function ()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
---                "gopls",
---                "clangd",
---                "cmake",
-                "grammarly",
-                "jsonls",
-                "jdtls",
-                "ts_ls",
-                "texlab",
-                "marksman",
---                "jedi_language_server",
---                "sqls",
-                "bashls",
+      require("fidget").setup({})
+      require("mason").setup()
+      require("mason-lspconfig").setup{
+        automatic_enable = {
+          exclude = {
+            "jdtls"
+          }
+        }
+      }
 
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-                ["jdtls"] = function()
-                  return true
-                end,
-            }
-        })
 
         vim.diagnostic.config({
 
