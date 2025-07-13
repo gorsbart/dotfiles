46fb79388ed9d80d82439eb02abf473223ca936c
gorsbart <gorski.bartosz@icloud.com>
2025-07-14 01:56:42 +0200

    Add nvim surround

---
+++ config/.config/nvim/lua/plugins/nvim-surround.lua
@@ -0,0 +1,22 @@
+return {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+          keymaps = {
+            insert = "<C-z>",
+            insert_line = "<CS-z>",
+            normal = "yz",
+            normal_cur = "yzz",
+            normal_line = "yZ",
+            normal_cur_line = "yZZ",
+            visual = "Z",
+            visual_line = "gZ",
+            delete = "dz",
+            change = "cz",
+            change_line = "cZ",
+          },
+        })
+    end
+}
