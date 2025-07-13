646c38d919c1edc403e967e5ee04f62e11b7ac52
gorsbart <gorski.bartosz@icloud.com>
2025-07-14 01:57:10 +0200

    Add shortcut for editing register value

--- config/.config/nvim/lua/config/remap.lua
+++ config/.config/nvim/lua/config/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
 
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -10,3 +9,42 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>y", "\"+y")
+
+
+
+vim.keymap.set("n", "<leader>q", function ()
+  local timeout = vim.o.timeoutlen
+  local interval = 10
+  local waited = 0
+  local key = nil
+
+  vim.api.nvim_echo({ { "Waiting for register key...", "Question" } }, false, {})
+  -- Wait until key is pressed or timeout expires
+  while waited < timeout do
+    key = vim.fn.getchar(0)  -- non-blocking getchar
+    if key ~= 0 then break end
+    vim.cmd.sleep{args = {interval .. "m"}}
+    waited = waited + interval
+  end
+
+  if key == nil or key == 0 or key == 27 then
+    vim.api.nvim_echo({ { "", "Question" } }, false, {})
+    return
+  end
+
+  local reg = vim.fn.nr2char(key)
+  if not reg:match('^[0-9a-zA-Z#=*+/"-]$') then
+    vim.notify("Invalid register: " .. reg, vim.log.levels.WARN)
+    return
+  end
+  vim.ui.input({ prompt = 'Edit value in register ' .. reg .. ':', default=vim.fn.getreg(reg), cancelreturn=vim.fn.getreg(reg) }, function(input)
+    vim.fn.setreg(reg, input)
+  end)
+end
+, {desc = 'Register edit'})
+
+
+
+
+
+
