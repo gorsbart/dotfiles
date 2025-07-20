a532521716bbac7037f93ebf20f77d4cea5e87cf
gorsbart <gorski.bartosz@icloud.com>
2025-07-20 23:36:19 +0200

    Share data between neovim instances

--- config/.config/nvim/lua/config/events.lua
+++ config/.config/nvim/lua/config/events.lua
@@ -5,3 +5,20 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+
+vim.api.nvim_create_autocmd('FocusGained', {
+  desc = "Read shada file for project or common",
+  callback = function ()
+    vim.cmd.sleep("100m")
+    vim.cmd.rshada()
+  end,
+})
+
+
+vim.api.nvim_create_autocmd('FocusLost', {
+  desc = "Write shada file for project or common",
+  callback = function ()
+    vim.cmd.wshada()
+  end,
+})
