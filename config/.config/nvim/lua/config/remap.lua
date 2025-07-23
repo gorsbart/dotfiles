96a59d6f003d056492cd5c3bb424aab4053522e3
gorsbart <gorski.bartosz@icloud.com>
2025-07-24 00:31:28 +0200

    Make kitty pass cyoping to clipboard into neovim

--- config/.config/nvim/lua/config/remap.lua
+++ config/.config/nvim/lua/config/remap.lua
@@ -7,9 +7,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<leader>p", "\"_dP")
 vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>y", "\"+y")
-
 
 vim.keymap.set("i", "<C-e>", "<END>")
 
