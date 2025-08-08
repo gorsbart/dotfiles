212f96e52cb1763f2b9508027b50aa3a8768eb33
gorsbart <gorski.bartosz@icloud.com>
2025-08-08 19:38:46 +0200

    Swap tabs and fix theme

--- config/.config/nvim/lua/plugins/theme.lua
+++ config/.config/nvim/lua/plugins/theme.lua
@@ -4,7 +4,11 @@ return {
     priority = 1000,
     config = function ()
         require("catppuccin").setup({
-          transparent_background = true
+          transparent_background = true,
+          float = {
+            transparent = false, -- enable transparent floating windows
+            solid = false, -- use solid styling for floating windows, see |winborder|
+          },
         })
 
         vim.cmd.colorscheme "catppuccin-mocha"
